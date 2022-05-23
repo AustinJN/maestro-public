5daa884cb25706b7c52003ec6657d95f2c2c975c
Austin Nelson <austinjn@uci.edu>
2022-05-23 14:29:20 -0700

    Reset

--- tasks/defense_project/submission/predict.py
+++ tasks/defense_project/submission/predict.py
@@ -54,9 +54,13 @@ class Prediction():
         return model
 
     def preprocess(self, original_images):
-        blurrer = T.GaussianBlur(kernel_size=(1, 19), sigma=(0.1, 7))
+        """
+        blurrer = T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))
         blurred_img = blurrer(original_images)
         return blurred_img
+        """
+        image = torch.unsqueeze(original_images, 0)
+        return image
 
     def detect_attack(self, original_image):
         return False
