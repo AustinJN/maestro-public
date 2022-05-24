82d1a6e120bf997f5466708334076728e5991030
Austin Nelson <austinjn@uci.edu>
2022-05-23 18:35:22 -0700

    Best Training

--- tasks/defense_project/submission/train.py
+++ tasks/defense_project/submission/train.py
@@ -86,20 +86,10 @@ class Adv_Training():
                 
                 perturbed_nonFGSM, _ = self.perturb_nontarget_FGSM.attack(inputs, labels.detach().cpu().tolist())
                 perturbed_nonFGSM = torch.tensor(perturbed_nonFGSM).to(device)
-                """
                 perturbed_FGSM, _ = self.perturb_target_FGSM.attack(inputs, labels.detach().cpu().tolist(), 0)
                 perturbed_FGSM = torch.tensor(perturbed_FGSM).to(device)
                 perturbed_PGD, _ = self.perturb_target_PGD.attack(inputs, labels.detach().cpu().tolist(), 0)
                 perturbed_PGD = torch.tensor(perturbed_PGD).to(device)
-                """
-                perturbed_FGSM = None
-                perturbed_PGD = None
-                if self.target_label:
-                    perturbed_FGSM, _ = self.perturb_target_FGSM.attack(inputs, labels.detach().cpu().tolist(), self.target_label)
-                    perturbed_FGSM = torch.tensor(perturbed_FGSM).to(device)
-                    perturbed_PGD, _ = self.perturb_target_PGD.attack(inputs, labels.detach().cpu().tolist(), self.target_label)
-                    perturbed_PGD = torch.tensor(perturbed_PGD).to(device)
-            
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -107,19 +97,12 @@ class Adv_Training():
                 loss = criterion(outputs, labels)
 
                 pert_output = self.model(perturbed_nonFGSM)
-                loss += criterion(pert_output, labels)
-                """
+                loss += criterion(pert_output, labels)  
                 pert_output = self.model(perturbed_FGSM)
                 loss += criterion(pert_output, labels)
                 pert_output = self.model(perturbed_PGD)
                 loss += criterion(pert_output, labels)
-                """
-                if self.target_label:
-                    pert_output = self.model(perturbed_FGSM)
-                    loss += criterion(pert_output, labels)
-                    pert_output = self.model(perturbed_PGD)
-                    loss += criterion(pert_output, labels)
-                      
+
 
                 loss.backward()
                 optimizer.step()
