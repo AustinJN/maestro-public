bb36b716432b5c9ed14ff21c0bbf1bc1c0cc1bf3
Austin Nelson <austinjn@uci.edu>
2022-06-02 20:26:45 -0700

    PGD Change

--- tasks/defense_war/submission/train.py
+++ tasks/defense_war/submission/train.py
@@ -112,8 +112,8 @@ class Adv_Training():
                 perturbed_nonFGSM = torch.tensor(perturbed_nonFGSM).to(device)
                 perturbed_FGSM, _ = self.perturb_target_FGSM.attack(inputs, labels.detach().cpu().tolist(), 0)
                 perturbed_FGSM = torch.tensor(perturbed_FGSM).to(device)
-                perturbed_PGD, _ = self.perturb_target_PGD.attack(inputs, labels.detach().cpu().tolist(), 0)
-                perturbed_PGD = torch.tensor(perturbed_PGD).to(device)
+                #perturbed_PGD, _ = self.perturb_target_PGD.attack(inputs, labels.detach().cpu().tolist(), 0)
+                #perturbed_PGD = torch.tensor(perturbed_PGD).to(device)
 
                 optimizer.zero_grad()
                 outputs = self.model(inputs)
@@ -123,8 +123,8 @@ class Adv_Training():
                 loss += criterion(pert_output, labels) * 0.5 
                 pert_output = self.model(perturbed_FGSM)
                 loss += criterion(pert_output, labels)
-                pert_output = self.model(perturbed_PGD)
-                loss += criterion(pert_output, labels)
+                #pert_output = self.model(perturbed_PGD)
+                #loss += criterion(pert_output, labels)
 
                 loss.backward()
                 optimizer.step()
